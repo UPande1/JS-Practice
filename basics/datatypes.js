8c1df6d149a2f2de8f9a5d160e194a3b5124e7e3
Utkarsh Pande <95009270+UPande1@users.noreply.github.com>
2024-01-14 07:26:18 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+let isLoggedIn = false
+let age = 24
+let name = "Utkarsh"
+let temp = null
+let state;
+
+console.table([isLoggedIn, age,name, temp])
+
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof state)
+
+/*
+int => 2 ^ 53
+bigInt 
+String => ""
+null => the type of null is an object
+state undefined
+BOOLEAN => true/false
+
+symbol => unique
+*/
