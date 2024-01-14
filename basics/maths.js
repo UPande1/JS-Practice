c46dbf4ec1b6e5c32d6902fa08d50eebea6da738
Utkarsh Pande <95009270+UPande1@users.noreply.github.com>
2024-01-14 14:20:58 +0000

    math in Javascript

---
+++ basics/maths.js
@@ -0,0 +1,13 @@
+let score = 100
+let balance = new Number (100)
+
+// console.log(balance);
+// console.log(score);
+
+let num = 123.837
+
+console.log(Math.random());
+let min = 1
+let max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
