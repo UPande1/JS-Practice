7f7e051c7a94831fa25a35957935734a788f0506
Utkarsh Pande <95009270+UPande1@users.noreply.github.com>
2024-01-14 07:57:51 +0000

    conversion and comparison basics

---
+++ basics/conversion.js
@@ -0,0 +1,46 @@
+let score = "33abc"
+
+let stringNumber = Number(score)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// if there is an alphanumeric value, and is to be coneverted into a number, it returns its
+// type as a number butlogs out NaN (not a number) as an output
+
+let ans = 5
+let booleanVal = Boolean(ans)
+// console.log(booleanVal)
+// console.log(typeof booleanVal)
+
+/*
+if
+ans = 1 => true
+ans = 0 => false
+ans = "Utkarsh => true"
+
+*/
+
+
+/*  ******************************Operations*************************************   */
+
+let str1 = "1"
+let str2 = "2"
+
+let num = 2
+let str3 = str1 + num
+
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
