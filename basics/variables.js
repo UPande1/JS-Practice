7e5e2c30313149144916554fce8c349f72b5a72b
Utkarsh Pande <95009270+UPande1@users.noreply.github.com>
2024-01-14 07:24:06 +0000

    datatypes

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "utkarshpande342@gmail.com"
+var accountPass = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 713344
+accountEmail = "oah@jmailc.com"
+accountPass = "ajhef"
+accountCity = "Mumbai"
+
+console.table ([accountEmail,accountId, accountPass, accountCity, accountState]);
