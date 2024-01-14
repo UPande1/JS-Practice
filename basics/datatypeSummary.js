2c7372eeda409e0b65967caa201bd293dcff8503
Utkarsh Pande <95009270+UPande1@users.noreply.github.com>
2024-01-14 08:13:29 +0000

    summary

---
+++ basics/datatypeSummary.js
@@ -0,0 +1,10 @@
+//datatypes are catagorised into two types
+// primitive datatypes => they are called by value
+
+// 7 => Number, String, Null, Symbol, bigInt, boolean, undefined
+
+
+// non primitive => they are called by referrance
+// arrays, functions, objects
+
+//read documentation at mdn
